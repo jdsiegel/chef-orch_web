fd15b4f55c0dd4eb30301350df573e6b0941e686
Jeff Siegel <jdsiegel@gmail.com>
2012-10-18 21:16:41 -0600

    require a upstream server list
    Notify with 'reload' again, assuming the runit fix to the nginx cookbook goes through

--- libraries/wub.rb
+++ libraries/wub.rb
@@ -1,14 +1,15 @@
 module Wub
   def configure_app(app)
     name      = app['name']
-    servers   = app['servers']  || []
-    hostname  = app['hostname'] || "_"
-    port      = app['port']     || 80
+    servers   = app['servers']           || []
+    hostname  = app['hostname']          || "_"
+    port      = app['port']              || 80
     root_path = app['root_path']
     log_path  = node['nginx']['log_dir']
 
     raise "App requires a name" unless name
     raise "App '#{name}' requires a root path" unless root_path
+    raise "App '#{name}' has an empty server list" if servers.empty?
 
     servers = servers.split if servers.respond_to?(:split)
 
@@ -26,9 +27,9 @@ module Wub
       })
 
       # doesn't work with nginx via runit. runit definition does not enable "reload" action
-      #notifies     :reload, "service[nginx]"
+      notifies     :reload, "service[nginx]"
 
-      notifies     :restart, "service[nginx]"
+      #notifies     :restart, "service[nginx]"
     end
 
     nginx_site "#{name}"
