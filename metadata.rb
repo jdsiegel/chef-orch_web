f55271eeabd2209818570fe9344af59ac5008a88
Jeff Siegel <jdsiegel@gmail.com>
2013-05-05 00:59:47 -0600

    depend on runit cookbook
    The runit_service resource is not available unless runit is an explicit
    dependency

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description      "Opinionated nginx reverse proxy for rails/rack apps"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
+depends "runit"
 depends "nginx"
 
 supports "ubuntu"
