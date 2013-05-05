48ba8d009ea4780fa2d82a48585932b4763a6535
Jeff Siegel <jdsiegel@gmail.com>
2013-05-05 01:56:29 -0600

    silence curl

--- test/integration/ssl/shunit2/apps_test.sh
+++ test/integration/ssl/shunit2/apps_test.sh
@@ -2,21 +2,21 @@
 
 test_app_is_running_on_https()
 {
-  curl https://localhost --cacert /etc/nginx/ssl/sslapp.cert > /dev/null
+  curl -s https://localhost --cacert /etc/nginx/ssl/sslapp.cert > /dev/null
 
   assertTrue "could not connect to https port" $?
 }
 
 test_app_root_page_contains_nginx_default_markup()
 {
-  curl https://localhost --cacert /etc/nginx/ssl/sslapp.cert | grep "Welcome to nginx!" > /dev/null
+  curl -s https://localhost --cacert /etc/nginx/ssl/sslapp.cert | grep "Welcome to nginx!" > /dev/null
 
   assertTrue "nginx markup was not found" $?
 }
 
 test_app_serves_static_files_from_root_path()
 {
-  local output=$(curl https://localhost/notes.txt --cacert /etc/nginx/ssl/sslapp.cert)
+  local output=$(curl -s https://localhost/notes.txt --cacert /etc/nginx/ssl/sslapp.cert)
 
   assertEquals "This should be served up" "$output"
 }
--- test/integration/standard/shunit2/apps_test.sh
+++ test/integration/standard/shunit2/apps_test.sh
@@ -2,42 +2,42 @@
 
 test_app1_runs_on_port_80()
 {
-  curl http://localhost > /dev/null
+  curl -s http://localhost > /dev/null
 
   assertTrue "could not connect to port 80" $?
 }
 
 test_app1_root_page_contains_nginx_default_markup()
 {
-  curl http://localhost | grep "Welcome to nginx!" > /dev/null
+  curl -s http://localhost | grep "Welcome to nginx!" > /dev/null
 
   assertTrue "nginx markup was not found" $?
 }
 
 test_app1_serves_static_files_from_root_path()
 {
-  local output=$(curl http://localhost/notes.txt)
+  local output=$(curl -s http://localhost/notes.txt)
 
   assertEquals "This should be served up" "$output"
 }
 
 test_app2_runs_on_port_81()
 {
-  curl http://localhost:81 > /dev/null
+  curl -s http://localhost:81 > /dev/null
 
   assertTrue "could not connect to port 81" $?
 }
 
 test_app2_root_page_contains_nginx_default_markup()
 {
-  curl http://localhost:81 | grep "Welcome to nginx!" > /dev/null
+  curl -s http://localhost:81 | grep "Welcome to nginx!" > /dev/null
 
   assertTrue "nginx markup was not found" $?
 }
 
 test_app2_serves_static_files_from_root_path()
 {
-  local output=$(curl http://localhost:81/notes.txt)
+  local output=$(curl -s http://localhost:81/notes.txt)
 
   assertEquals "This should be served up" "$output"
 }
